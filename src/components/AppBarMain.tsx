372c5cbb14377c80a6b9dc60a31b25dbb9907466
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-08-23 20:39:18 +0200

    feat(page): loader

--- src/components/AppBarMain.tsx
+++ src/components/AppBarMain.tsx
@@ -6,9 +6,6 @@ export const AppBarMain: React.FC = () => {
     return (
         <AppBar>
             <AppBar.Toolbar>
-                <AppBar.ToolbarLead>
-                    <ArrowLeft size={24} />
-                </AppBar.ToolbarLead>
                 <AppBar.ToolbarCenter classes="hidden sm:block">
                     <h1 className="type-scale-9 font-bold uppercase text-9xl">
                       <span
@@ -17,20 +14,7 @@ export const AppBarMain: React.FC = () => {
                      </span>
                     </h1>
                 </AppBar.ToolbarCenter>
-                <AppBar.ToolbarTrail>
-                    <div className="hidden space-x-4 sm:flex">
-                        <Paperclip size={20}/>
-                        <Calendar size={20}/>
-                        <CircleUser size={20}/>
-                    </div>
-                    <div className="block sm:hidden">
-                        <Menu size={20} />
-                    </div>
-                </AppBar.ToolbarTrail>
             </AppBar.Toolbar>
-            <AppBar.Headline classes="sm:hidden">
-                <h2 className="h2">Title</h2>
-            </AppBar.Headline>
         </AppBar>
     );
 };
