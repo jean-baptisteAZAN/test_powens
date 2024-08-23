e4015c5d92a35564964c234efd731e9bbd555fb5
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-08-23 21:12:57 +0200

    feat(loginbutton)

--- src/components/AppBarMain.tsx
+++ src/components/AppBarMain.tsx
@@ -6,7 +6,7 @@ export const AppBarMain: React.FC = () => {
     return (
         <AppBar>
             <AppBar.Toolbar>
-                <AppBar.ToolbarCenter classes="hidden sm:block">
+                <AppBar.ToolbarCenter classes="block">
                     <h1 className="type-scale-9 font-bold uppercase text-9xl">
                       <span
                           className="bg-gradient-radial from-tertiary-500 to-primary-500 box-decoration-clone bg-clip-text text-transparent">
